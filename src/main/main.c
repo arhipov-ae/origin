b1e096145b9fb862e83c985b803f075e99a94340
arhipov-ae <55047301+arhipov-ae@users.noreply.github.com>
2020-03-17 17:18:08 +0300

    Add files via upload

---
+++ src/main/main.c
@@ -0,0 +1,111 @@
+#include "mcu_support_package/inc/stm32f10x.h"
+
+void configure_USART(void);
+void configure_GPIO(void);
+void configure_NVIC(void);	
+void USART1_IRQHandler(void);
+
+int main()
+{
+	__enable_irq(); 
+	configure_USART();
+	configure_GPIO();
+	configure_NVIC();
+	
+	while(1)
+  {
+	
+  }
+}
+
+void configure_USART(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+	
+	USART_InitTypeDef USART_InitStruct;
+	
+	USART_InitStruct.USART_BaudRate = 9600;
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_InitStruct.USART_Parity = USART_Parity_No;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	
+	USART_Init(USART1, &USART_InitStruct);
+	USART_Cmd(USART1, ENABLE);
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+}
+
+void configure_GPIO(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  GPIO_InitTypeDef GPIO_InitStructure;
+ 
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_8;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
+  GPIO_Init(GPIOA, &GPIO_InitStructure); 
+ 
+ 
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+ 
+  
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+void configure_NVIC(void)
+{
+	NVIC_InitTypeDef NVIC_InitStruct;
+	
+	NVIC_InitStruct.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+	
+	NVIC_Init(&NVIC_InitStruct);
+}
+
+
+void USART1_IRQHandler(void)
+{
+    int i;
+    if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+    {
+        USART_ClearFlag(USART1, USART_IT_RXNE);
+			i = USART_ReceiveData(USART1);
+        if(i == 1)
+          GPIO_WriteBit(GPIOA,GPIO_Pin_8,Bit_SET);        
+        else if(i == 0)
+          GPIO_WriteBit(GPIOA,GPIO_Pin_8,Bit_RESET);    
+                
+    }
+}
+ 
+
+#ifdef USE_FULL_ASSERT
+
+// эта функция вызывается, если assert_param обнаружил ошибку
+void assert_failed(uint8_t * file, uint32_t line)
+{ 
+    /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+     
+    (void)file;
+    (void)line;
+
+    __disable_irq();
+    while(1)
+    {
+        // это ассемблерная инструкция "отладчик, стой тут"
+        // если вы попали сюда, значит вы ошиблись в параметрах вызова функции из SPL. 
+        // Смотрите в call stack, чтобы найти ее
+        __BKPT(0xAB);
+    }
+}
+
+#endif
