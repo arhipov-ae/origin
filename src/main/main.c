6736e5a1db7b42154a4e14e93e55c218dfab7f29
arhipov-ae <55047301+arhipov-ae@users.noreply.github.com>
2020-03-17 17:41:30 +0300

    Update main.c

--- src/main/main.c
+++ src/main/main.c
@@ -38,24 +38,24 @@ void configure_USART(void)
 
 void configure_GPIO(void)
 {
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-  GPIO_InitTypeDef GPIO_InitStructure;
+ 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  	GPIO_InitTypeDef GPIO_InitStructure;
  
-  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_8;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
-  GPIO_Init(GPIOA, &GPIO_InitStructure); 
+  	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_8;
+  	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
+  	GPIO_Init(GPIOA, &GPIO_InitStructure); 
  
  
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+  	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  	GPIO_Init(GPIOA, &GPIO_InitStructure);
  
   
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+  	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  	GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
 void configure_NVIC(void)
@@ -73,17 +73,17 @@ void configure_NVIC(void)
 
 void USART1_IRQHandler(void)
 {
-    int i;
-    if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
-    {
-        USART_ClearFlag(USART1, USART_IT_RXNE);
-	i = USART_ReceiveData(USART1);
-        if(i == 1)
-          GPIO_WriteBit(GPIOA,GPIO_Pin_8,Bit_SET);        
-        else if(i == 0)
-          GPIO_WriteBit(GPIOA,GPIO_Pin_8,Bit_RESET);    
+    	int i;
+    	if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+    	{
+        	USART_ClearFlag(USART1, USART_IT_RXNE);
+		i = USART_ReceiveData(USART1);
+        	if(i == 1)
+          		GPIO_WriteBit(GPIOA,GPIO_Pin_8,Bit_SET);        
+        	else if(i == 0)
+          		GPIO_WriteBit(GPIOA,GPIO_Pin_8,Bit_RESET);    
                 
-    }
+    	}
 }
  
 
